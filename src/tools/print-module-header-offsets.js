61db4bcbcbf77aab3ecbaefd0a710f36a96283c5
Remko Tronçon <remko@el-tramo.be>
2019-11-09 20:40:23 +0100

    Add WIP file

---
+++ src/tools/print-module-header-offsets.js
@@ -0,0 +1,8 @@
+TODO
+;;;   !moduleHeaderSize := (string-length !moduleHeader))
+;;;   !moduleHeaderCodeSizeOffset := (char-index (string->list !moduleHeader) \FF 0))
+;;;   !moduleHeaderBodySizeOffset := (char-index (string->list !moduleHeader) \FE 0))
+;;;   !moduleHeaderLocalCountOffset := (char-index (string->list !moduleHeader) \FD 0))
+;;;   !moduleHeaderTableIndexOffset := (char-index (string->list !moduleHeader) \FC 0))
+;;;   !moduleHeaderTableInitialSizeOffset := (char-index (string->list !moduleHeader) \FB 0))
+;;;   !moduleHeaderFunctionTypeOffset := (char-index (string->list !moduleHeader) \FA 0))
