b9bcce1026c30fc8c2343673edf37d17b48a7f01
Remko Tronçon <remko@el-tramo.be>
2022-04-23 23:18:50 +0200

    build: return content-type in static server
    This makes sure the index works on Chrome.

--- build-web.js
+++ build-web.js
@@ -131,7 +131,10 @@ if (watch) {
     }
     try {
       const data = await fs.promises.readFile(f);
-      res.writeHead(200);
+      res.writeHead(
+        200,
+        f.endsWith(".html") ? { "Content-Type": "text/html" } : undefined
+      );
       res.end(data);
     } catch (err) {
       res.writeHead(404);
