c013b2493ded9e4be644b89b9ca74ac62019a7ee
Remko Tronçon <remko@el-tramo.be>
2022-05-23 20:08:47 +0200

    Revert "Grow table from within WebAssembly"
    It doesn't work in Node 16 (used by GitHub actions)
    
    This reverts commit 2747458242c27c6bcf2474a3f760c26cd8f2e9f2.

--- src/web/waforth.ts
+++ src/web/waforth.ts
@@ -147,6 +147,9 @@ class WAForth {
             }
             data = new Uint8Array(dataCopy);
           }
+          if (index >= table.length) {
+            table.grow(table.length); // Double size
+          }
           // console.log("Load", index, arrayToBase64(data));
           try {
             var module = new WebAssembly.Module(data);
